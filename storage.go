87dc01bd9e9cea78b66579ed423cc779dacbfeeb
Altusha4 <240782@astanait.edu.kz>
2026-01-02 22:39:00 +0600

    add timetable storage constructor

--- storage.go
+++ storage.go
@@ -6,3 +6,9 @@ type TimetableStorage struct {
 	mu   sync.RWMutex
 	data map[string]TimetableEntry
 }
+
+func NewTimetableStorage() *TimetableStorage {
+	return &TimetableStorage{
+		data: make(map[string]TimetableEntry),
+	}
+}
