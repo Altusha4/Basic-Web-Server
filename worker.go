23cdad0b0415811ed3e33094813e444152bd3837
Altusha4 <240782@astanait.edu.kz>
2026-01-02 23:10:06 +0600

    add background worker for timetable monitoring

--- worker.go
+++ worker.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"context"
+	"log"
+	"time"
+)
+
+func StartBackgroundWorker(
+	ctx context.Context,
+	service *TimetableService,
+) {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			requests, entries := service.Stats()
+			log.Printf(
+				"Timetable status: requests=%d, entries=%d",
+				requests,
+				entries,
+			)
+
+		case <-ctx.Done():
+			log.Println("Background worker stopped")
+			return
+		}
+	}
+}
