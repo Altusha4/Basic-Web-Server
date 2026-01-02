18a00f3ea3dad141c00bca24319141c61dfbbd6d
Altusha4 <240782@astanait.edu.kz>
2026-01-02 22:33:12 +0600

    add timetable entry model

--- model.go
+++ model.go
@@ -1,1 +1,10 @@
 package main
+
+type TimetableEntry struct {
+	ID      string `json:"id"`
+	Subject string `json:"subject"`
+	Day     string `json:"day"`
+	Time    string `json:"time"`
+	Room    string `json:"room"`
+	Teacher string `json:"teacher"`
+}
