60d7071c1dc57a89bf624054c9804ed7cfa29425
Altusha4 <240782@astanait.edu.kz>
2026-01-02 22:46:51 +0600

    add POST /timetable handler

--- handler.go
+++ handler.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type TimetableHandler struct {
 	service *TimetableService
 }
@@ -8,4 +13,16 @@ func NewTimetableHandler(service *TimetableService) *TimetableHandler {
 	return &TimetableHandler{
 		service: service,
 	}
+}
+
+func (h *TimetableHandler) CreateTimetable(w http.ResponseWriter, r *http.Request) {
+	var entry TimetableEntry
+
+	if err := json.NewDecoder(r.Body).Decode(&entry); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	h.service.AddEntry(entry)
+	w.WriteHeader(http.StatusCreated)
 }
