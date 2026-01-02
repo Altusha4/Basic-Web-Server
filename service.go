efcbc790cc81c5f5fc7581271268dd2ae9e16c7d
Altusha4 <240782@astanait.edu.kz>
2026-01-02 22:43:25 +0600

    add service statistics logic

--- service.go
+++ service.go
@@ -26,3 +26,7 @@ func (s *TimetableService) DeleteEntry(id string) bool {
 	s.totalRequests++
 	return s.storage.Delete(id)
 }
+
+func (s *TimetableService) Stats() (int, int) {
+	return s.totalRequests, s.storage.Count()
+}
