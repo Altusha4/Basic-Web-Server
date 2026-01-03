4b1a81abc1456f3138504bb9bc04f5519a0a04ca
Altynay <240782@astanait.edu.kz>
2026-01-03 11:37:39 +0000

    simplify service layer to basic timetable operations

--- service.go
+++ service.go
@@ -2,8 +2,6 @@ package main
 
 type TimetableService struct {
 	storage *TimetableStorage
-
-	totalRequests int
 }
 
 func NewTimetableService(storage *TimetableStorage) *TimetableService {
@@ -13,20 +11,17 @@ func NewTimetableService(storage *TimetableStorage) *TimetableService {
 }
 
 func (s *TimetableService) AddEntry(entry TimetableEntry) {
-	s.totalRequests++
 	s.storage.Add(entry)
 }
 
 func (s *TimetableService) GetAllEntries() map[string]TimetableEntry {
-	s.totalRequests++
 	return s.storage.GetAll()
 }
 
 func (s *TimetableService) DeleteEntry(id string) bool {
-	s.totalRequests++
 	return s.storage.Delete(id)
 }
 
-func (s *TimetableService) Stats() (int, int) {
-	return s.totalRequests, s.storage.Count()
+func (s *TimetableService) Count() int {
+	return s.storage.Count()
 }
